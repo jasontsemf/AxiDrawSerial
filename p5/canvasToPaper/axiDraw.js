97a2d47d02f2dfebdeaf382f259bb344a857ea51
Jason <jasontsemf@gmail.com>
2021-04-10 02:03:08 -0400

    Update axiDraw.js

--- p5/canvasToPaper/axiDraw.js
+++ p5/canvasToPaper/axiDraw.js
@@ -30,6 +30,29 @@ function move(ms, x, y) {
     sendCommand(cmd);
 }
 
+function moveAlong(p) {
+    // compute XY delta values with given XY coordinates 
+    let output = [];    // array for holding XY delta and timing value
+    let x, y, ms, xOrY;
+    for (i = 0; i < p.length; i++) {
+        if (i - 1 >= 0) {   // current point that has a previous point
+            // calculate step differences
+            x = p[i][0] - p[i - 1][0];
+            y = p[i][1] - p[i - 1][1];
+        } else {
+            x = p[i][0];
+            y = p[i][1];
+        }
+        xOrY = Math.abs(x) > Math.abs(y) ? x : y;   // find the longer axis
+        ms = Math.floor(((Math.abs(xOrY) / 1.31 - Math.abs(xOrY) / 25)) * 0.3);     // limit movement speed to 30% of the operating speed range
+        output.push([ms, x, y]);
+    }
+    for (i = 0; i < output.length; i++) {
+        let cmd = `XM,${output[i][0]},${output[i][1]},${output[i][2]}`;
+        sendCommand(cmd);
+    }
+}
+
 function keyboardMove(s){
     if ((keyIsDown(87) && keyIsDown(65)) || keyIsDown(UP_ARROW) && keyIsDown(LEFT_ARROW)) {
         console.log("NW");
